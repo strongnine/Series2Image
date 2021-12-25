a3d3b37c2b44285486bcb86c436ccb133ce501ed
strongnine <strongnine@163.com>
2021-12-25 13:56:19 +0800

    debug paa.py

--- paa.py
+++ paa.py
@@ -5,14 +5,15 @@ from pyts.approximation import PiecewiseAggregateApproximation
 data = np.loadtxt("data.csv", delimiter=',')
 data = data.reshape(1, -1)
 n_sampels, n_timestamps = data.shape
-n_paa = 512
+n_paa = 128
 window_size = n_timestamps // n_paa
 paa = PiecewiseAggregateApproximation(window_size=window_size)
 X_paa = paa.transform(data)[:, :n_paa]
-np.savetxt("./paa_result.csv", X_paa.T, fmt='%.18f')
+# np.savetxt("./paa_result.csv", X_paa.T, fmt='%.18f')
 
-plt.figure(figsize=(18, 12))
-plt.plot(data[0], 'o--', ms=0.1, label='Original', linewidth=0.1)
+plt.figure(figsize=(6, 4))
+plt.plot(data[0], 'o--', ms=0.1, label='Original', linewidth=1.)
 plt.plot(np.arange(window_size // 2,
                    n_timestamps + window_size // 2,
-                   window_size)[:n_paa], X_paa[0], 'o--', ms=2.5, label='PAA', linewidth=0.2)
+                   window_size)[:n_paa], X_paa[0], 'o--', ms=2.5, label='PAA', linewidth=1.)
+plt.show()
